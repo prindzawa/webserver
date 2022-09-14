d3e150fa0a43b220615160b64e1cd5d7cc6b1cd3
prindzawa <113496231+prindzawa@users.noreply.github.com>
2022-09-13 23:30:13 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Form</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+<!-- <h2>Login Form</h2> -->
+
+<form action="tampil.php" method="POST">
+<h2>Login Form</h2>
+<div class="imgcontainer">
+<img src="icon.png" alt="Avatar" class="avatar">
+</div>
+
+<div class="container">
+<label><b>Username</b></label>
+<input type="text" placeholder="Masukkan Username" name="username" required>
+<label><b>Password</b></label>
+<input type="password" placeholder="Masukkan Password" name="email" required>
+
+<button type="submit">Masuk</button>
+<input type="checkbox" checked="checked"><span>Ingat Saya</span></div>
+</form>
+
+</body>
+</html>
+
